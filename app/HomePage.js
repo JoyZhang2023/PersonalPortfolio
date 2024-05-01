d9fd8203b92e652a02bf5ddfee92f40fa9706d94
JoyZhang2023 <jocelyn-zhang@live.com>
2024-04-30 18:04:26 -0700

    clean up

--- app/HomePage.js
+++ app/HomePage.js
@@ -1,14 +1,23 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ImageViewer from '../../ExpoConfigDemo/components/ImageViewer';
+import Button from '../../ExpoConfigDemo/components/Button';
+
+const PlaceholderImage = require('./assets/shibainu.png');
 
 export default function HomePage() {
   return (
     <View style={styles.container}>
+
+      <View>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
       
       <Text>Home Page</Text>
-      <Text>Contact Information</Text>
+      <Link href={"/ContactPage"}>Contact Information</Link>
       <Link href={"/ProjectsPage"}>Projects Page</Link>
+      <Link href={"/SkillsPage"}>Skills</Link>
         
     </View>
   );
